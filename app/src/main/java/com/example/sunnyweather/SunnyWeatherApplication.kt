26443ceb8dd9c5ca136720bef079245b19947baf
bestyliu <1491777968@qq.com>
2020-12-31 16:46:15 +0800

    实现搜索全球城市数据功能

--- app/src/main/java/com/example/sunnyweather/SunnyWeatherApplication.kt
+++ app/src/main/java/com/example/sunnyweather/SunnyWeatherApplication.kt
@@ -3,9 +3,11 @@ package com.example.sunnyweather
 import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
+import android.media.session.MediaSession
 
 class SunnyWeatherApplication : Application(){
     companion object {
+        const val TOKEN = "jHU6KNKHFRmCxkMt"
         @SuppressLint("StaticFieldLeak")
         lateinit var context: Context
     }
